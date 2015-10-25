2561904625e55ac985a1bf2ea055de32903b7846
archer <1246506786@qq.com>
2015-10-25 07:12:57 -0400

    git ignore

--- docs/resources.md
+++ docs/resources.md
@@ -2,4 +2,6 @@
 [MongoDB Tutorial](http://www.tutorialspoint.com/mongodb/)  
 [mongodb insert time](http://stackoverflow.com/questions/19612338/inserting-the-current-datetime-in-mongodb)  
 [mongodb insert location data](http://stackoverflow.com/questions/15274834/how-to-store-geospatial-information-in-mongodb)  
+[My Adventures in Coding](https://myadventuresincoding.wordpress.com/2011/10/02/mongodb-geospatial-queries/)  
+
 
--- main.go
+++ main.go
@@ -6,8 +6,32 @@ import (
 
 func main() {
   m := martini.Classic()
+
+  m.Put("/gps", func() string {
+    return "create gps data"
+  })
+
+  m.Get("/gps", func() string {
+    return "get gps data"
+  })
+  // m.Get("/gps/:id", func() string {
+  //   return "get a specific gps data"
+  // })
+  // m.Get("/gps/:imei", func() string {
+  //   return "get gps data related to a device"
+  // })
+
+  m.Post("/gps", func() string {
+    return "update gps data"
+  })
+
+  m.Delete("/gps", func() string {
+    return "delete gps data"
+  })
+
   m.Get("/", func() string {
     return "Hello world!"
   })
+
   m.Run()
 }
