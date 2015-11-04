b6eb2b64f4d2c6cb10a602c99895839666813ec9
dishytianxiang <1066844321@qq.com>
2015-11-04 16:51:30 +0800

    add passwd md5

--- controllers/login.go
+++ controllers/login.go
@@ -2,16 +2,20 @@ package controllers
 
 import (
 	"Shepherd/models"
+	"Shepherd/services"
 	"fmt"
-
 	"github.com/martini-contrib/render"
 )
 
 func UserLogin(r render.Render, user models.User) {
+	fmt.Println(user.Email, " ", user.Passwd)
 	if user.Email == "1066844321@qq.com" {
+		services.AddSession(user.Email)
+		fmt.Println("if ", services.ReturnSession())
 		r.HTML(200, "main", nil)
 	} else {
+		fmt.Println("else ", services.ReturnSession())
 		r.HTML(404, "404", nil)
 	}
-	fmt.Println(user)
+
 }
